cdb36d088f8b89826774a11329f2fecc6a9834c4
Fl4tt3rF0tz3 <fabiwolter@googlemail.com>
2015-11-03 17:05:39 +0100

    test

---
+++ workspace/Test/src/uulm/kack/test/TestKlasse.java
@@ -0,0 +1,12 @@
+package uulm.kack.test;
+
+import java.net.ServerSocket;
+
+public class TestKlasse {
+	public static void main(String[] args) {
+		System.out.println("penis");
+
+		ServerSocket superkack = null;
+		ArrayIndexOutOfBoundsException a;
+	}
+}
